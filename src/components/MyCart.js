378ff9cd87e255fcfac0f7386993998ab0bae1d1
aashabul <aashabulimam@gmail.com>
2022-03-19 01:00:03 +0600

    updated

--- src/components/MyCart.js
+++ src/components/MyCart.js
@@ -1,6 +1,16 @@
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { IconButton, Tooltip } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const MyCart = () => {
   const { user } = useAuth();
@@ -14,14 +24,76 @@ const MyCart = () => {
       );
   }, [user.email]);
 
+  const handleAddWishlist = (e) => {
+    console.log("added to wishlist");
+  };
+
+  const handleConfirmOrder = (e) => {
+    console.log("order confirmed");
+  };
+
+  const handleDeleteOrder = (e) => {
+    console.log("order deleted from cart");
+  };
+
   return (
     <div>
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Confirm Order{" "}
+        <span style={{ color: "green" }}>({cart.length} Items)</span>
+      </Typography>
       {cart.map((cart, index) => (
-        <Box key={index}>
-          <h3>ProductId: {cart._id}</h3>
-          <h3>Name: {cart.name}</h3>
-          <h3>Price: ${cart.price}</h3>
-        </Box>
+        <List
+          key={index}
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            my: 3,
+          }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt={cart.name} src={cart.img} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={cart.name}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    price: $ {cart.price}
+                  </Typography>
+                  <Typography
+                    sx={{ display: "block" }}
+                    component="span"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    color: {cart.color}
+                  </Typography>
+                  <>
+                    <Tooltip title="continue to shipping">
+                      <IconButton sx={{ mr: 5 }} onClick={handleAddWishlist}>
+                        <FavoriteBorderIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="delete from cart">
+                      <IconButton onClick={handleDeleteOrder}>
+                        <DeleteForeverIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </>
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="middle" component="li" />
+        </List>
       ))}
     </div>
   );
